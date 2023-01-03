c9f346977c683446cd5fb7062e0010535e579941
student-14 <terleckawika123@gmail.com>
2023-01-03 13:02:46 +0100

    Przycisk otwiera kamerę

--- app/src/main/java/com/pablo/pic2/MainActivity.java
+++ app/src/main/java/com/pablo/pic2/MainActivity.java
@@ -2,13 +2,29 @@ package com.pablo.pic2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView imageView1;
+Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView1 = findViewById(R.id.imageView1);
+        button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(open_camera, 100);
+            }
+        });
     }
 }
